aa4b3d1898eda06b71d72e9bb95a7832111a4696
muneebazeem3 <55459043+muneebazeem3@users.noreply.github.com>
2019-10-06 13:25:32 +0500

    Add files via upload

---
+++ Module1.vb
@@ -0,0 +1,30 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim str1, str2, char1, nextchar As String
+        Dim counter As Integer
+
+        str1 = ""
+        str2 = ""
+        char1 = ""
+        nextchar = ""
+
+        Console.Write("Enter the string: ")
+        str1 = Console.ReadLine
+
+        Console.Write("Enter character to remove: ")
+        char1 = Console.ReadLine
+
+        For counter 1 to Len(str1)
+            nextchar = Mid(str1, counter, 1)
+            If nextchar <> char1 Then
+                str2 = str2 & nextchar
+            End If
+        Next
+
+        Console.WriteLine("final string: " & str2)
+        Console.ReadKey()
+
+    End Sub
+
+End Module
